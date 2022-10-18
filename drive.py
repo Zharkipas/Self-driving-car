dbc7d8f2e64eb19a20841c69e06c257cf20a9279
Zharkipas <zharbiter617@gmail.com>
2022-10-18 11:07:53 +0800

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__) #'_amin_'
+app = Flask(__name__) #'_main_'
 
 @app.route('/home')
 def greeting():
     return 'Welcome'
 
-if __name__ == '_main':
+if __name__ == '_main_':
     app.run(port=3000)
